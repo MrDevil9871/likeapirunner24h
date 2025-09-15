b6bd54d7e2352273cf362a7733926ddaf16d2db0
MrDevil9871 <mk0434067@gmail.com>
2025-09-15 14:09:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import json
 import requests
 import datetime
-from telegram import Update
+from telegram 
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # 🔑 Env vars se secure values
